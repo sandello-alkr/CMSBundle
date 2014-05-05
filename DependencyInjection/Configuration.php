df34240e023e6e27be5ce9a52933946ef8d21815
alkr <sandello.alkr@gmail.com>
2014-05-05 13:54:26 +0300

    Вытеснение globals в конфиги, описание сервисов внутри бандла
    Signed-off-by: alkr <sandello.alkr@gmail.com>

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,6 +20,32 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('cms');
 
+        $rootNode
+            ->children()
+                ->scalarNode('children_type')->defaultValue('list')->end()
+                ->scalarNode('base_template')
+                    ->defaultValue('two_sidebars.html.twig')
+                    ->validate()
+                        ->ifNotInArray(array('two_sidebars.html.twig', 'one_sidebar.html.twig', 'full_width.html.twig'))
+                        ->thenInvalid('Invalid base template "%s"')
+                    ->end()
+                ->end()
+                ->scalarNode('url_by_path')->defaultValue('false')->end()
+                ->integerNode('top_menu_parent')->defaultValue(5)->end()
+                ->integerNode('left_menu_parent')->defaultValue(1)->end()
+                ->arrayNode('modules')
+                    ->children()
+                        ->booleanNode('feedback')->defaultValue('false')->end()
+                        ->booleanNode('map')->defaultValue('false')->end()
+                        ->booleanNode('gallery')->defaultValue('false')->end()
+                        ->booleanNode('redirects')->defaultValue('false')->end()
+                        ->booleanNode('posts')->defaultValue('false')->end()
+                        ->booleanNode('views')->defaultValue('false')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
---
+++ Twig/ConfigExposeExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace alkr\CMSBundle\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ConfigExposeExtension extends \Twig_Extension
+{
+    private $container;
+    
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+    
+    public function getGlobals()
+    {
+        // print_r ($this->container->getParameter('cms'));
+        return array(
+            // 'cms' => $this->container->getParameter('cms')
+        );
+    }
+    
+    public function getName()
+    {
+        return 'config_expose_extension';
+    }
+}
